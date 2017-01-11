47c6a047b4eaff1038af2c26fcf597f5594a5494
Tomisin <tomisinjenrola@gmail.com>
2017-01-11 19:53:57 -0330

    Created word arrays

---
+++ words.js
@@ -0,0 +1,78 @@
+var easy = {
+
+	"ọlá"    : "Wealth",
+	"ọla"    : "Tomorrow",
+	"òjò"    : "Rain",
+	"ọjọ"    : "Day",
+	"ògùn"   : "Medicine",
+	"ogun"   : "War",
+	"owó"    : "Money",
+	"ọwọ"    : "Hand",
+	"ajá"    : "Dog",
+	"abà"    : "Barn",
+	"apere"  : "Basket",
+	"ìsó"    : "Nail",
+	"ata"    : "Pepper",
+	"san-wó" : "Pay",
+	"bóyá"   : "Perhaps",
+	"èébú"   : "abuse",
+	"ogá"    : "Master",
+	"fífọ"   : "Ache",
+	"ẹbùn"   : "Award/Gift"
+
+};
+
+var medium = {
+
+	"ẹnikéni"  :	"Anybody",
+	"ìfarahàn" :	"Appearance",
+	"ìjiyàn"   :	"Argument",
+	"abíyá"    :	"Armpit",
+	"yíkákiri" :	"Move around",
+	"ìgberaga" :	"Arrogance/pride",
+	"agbẹjọrò" :	"Attorney",
+	"àláfíà"   :	"Good Health",
+	"dáríjì"   :	"Pardon",
+	"ògòngò"   :	"Ostrich",
+	"gboràn"   :	"Listen/Obey",
+	"ìsele"    :	"Occurrence",
+	"ìtojú"    :	"Care/Maintenance",
+	"ẹlòmíràn" :	"someone else",
+	"gúgúrú"   :	"popcorn",
+	"alágbára" :	"Powerful person",
+	"ọmọdé"    :	"Child",
+	"Ọgbọn"    :	"Wisdom",
+	"pátápátá" :	"absolutely",
+	"polówó"   :	"advertise",
+	"angeli"   :	"Angel"
+
+};
+
+var hard = {
+
+	"olùrànlọwọ" 														: "Aide",
+	"olódùmarè" 														: "Almighty",
+	"ìdáríjì ìjọba fún àwọn ti o ṣẹ ní ìlú" : "Amnesty",
+	"láìlórúkọ"        										  : "Anonymous",
+	"ọlọfà"            										  : "Archer",
+	"ìfetísílẹ"                             : "Attention",
+	"ìdánilójú"                             : "Assurance",
+	"ọgẹdẹ wẹrẹ"                            : "Banana",
+	"ajẹnirun-láìdáwó"                      : "Leech",
+	"òórùn dídùn"                           : "Perfume",
+	"òkúta were"                            : "Pebbles",
+	"sùúrù"                                 : "Patience",
+	"èlò ìdìmú ìwé"                         : "Paperclip",
+	"leekookan"                             : "Occasionally",
+	"àlùbosà"                               : "Onions",
+	"ìkójopo"                               : "Accumulation",
+	"ìyókù"                                 : "Remainder",
+	"ilé ìpamo owó"                         : "Bank",
+	"dalebi"                                : "Blame",
+	"àfowose"                               : "Manual",
+	"jàgídí-jàgan"                          : "Militant/Fighter",
+	"èdèkòyédè"                             : "Misunderstanding",
+	"ìjá nkan gbà nítipátipá"               : "Mugging",
+	"onígbèjà"                              : "Ally"
+
+};
