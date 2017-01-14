47beac2738f6c225371b9906e04354dbe307b39d
Tomisin <tomisinjenrola@gmail.com>
2017-01-14 18:18:29 -0330

    Created file for main script

---
+++ script.js
@@ -0,0 +1,65 @@
+// scroll down to page
+$(".header-scroll").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#main").offset().top},
+        'slow');
+});
+/////////////////////////////////////////////
+/////////////////////////////////////////////
+
+
+// scroll to top
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 50) 
+        $("#backToTop").fadeIn();
+    else
+        $("#backToTop").fadeOut();
+});
+
+$("#backToTop").click(function() {
+  $("html, body").animate({scrollTop: 10}, 800);
+  return false;
+});
+/////////////////////////////////////////////
+/////////////////////////////////////////////
+
+
+// waveform script
+var wavesurfer = WaveSurfer.create({
+    container: '#waveform',
+    waveColor: 'violet',
+    hideScrollbar: true
+});
+
+var microphone = Object.create(WaveSurfer.Microphone);
+
+microphone.init({
+    wavesurfer: wavesurfer
+});
+
+microphone.on('deviceReady', function(stream) {
+    console.log('Device ready!', stream);
+});
+microphone.on('deviceError', function(code) {
+    console.warn('Device error: ' + code);
+});
+
+// pause rendering
+//microphone.pause();
+
+// resume rendering
+//microphone.play();
+
+// stop visualization and disconnect microphone
+//microphone.stopDevice();
+
+// same as stopDevice() but also clears the wavesurfer canvas
+microphone.stop();
+
+// destroy the plugin
+//microphone.destroy();
+
+// wavesurfer.on('ready', function() {
+//  wavesurfer.zoom(100);
+//  console.log("zoomed");
+// });
