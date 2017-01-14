9e0cc4aed278bd1de6bfaa5d265ed7314cc3acfa
Tomisin <tomisinjenrola@gmail.com>
2017-01-14 18:18:51 -0330

    Started script for changing words

---
+++ play.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+	if($('input[name=lang]:checked').val() === "yoruba") {
+		console.log("yoruba");
+	}
+
+	if($('input[name=level]:checked').val() === "easy") {
+		console.log("ọwọ́");
+	}
+
+
+});
